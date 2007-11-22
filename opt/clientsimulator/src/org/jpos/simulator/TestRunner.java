be438d3ba1e968520cb653d9fdc35267a534c459
arevilla <arevilla@12794a80-bd30-0410-a029-31405052ec81>
2007-11-22 15:09:22 +0000

    upgrade to AGPL v3.0

--- opt/clientsimulator/src/org/jpos/simulator/TestRunner.java
+++ opt/clientsimulator/src/org/jpos/simulator/TestRunner.java
@@ -1,24 +1,21 @@
-/**
- *  jPOS Extended Edition
- *  Copyright (C) 2004 Alejandro P. Revilla
- *  jPOS.org (http://jpos.org)
+/*
+ * jPOS Project [http://jpos.org]
+ * Copyright (C) 2000-2007 Alejandro P. Revilla
  *
- *  This program is free software; you can redistribute it and/or modify
- *  it under the terms of the GNU General Public License as published by
- *  the Free Software Foundation; either version 2 of the License, or
- *  (at your option) any later version.
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
  *
- *  This program is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU General Public License for more details.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
  *
- *  You should have received a copy of the GNU General Public License
- *  along with this program; if not, write to the Free Software
- *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
- *
- *  $Id: TestRunner.java,v 1.2 2006/02/08 17:20:39 tlk Exp $
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
+
 package org.jpos.simulator;
 
 import java.io.File;
