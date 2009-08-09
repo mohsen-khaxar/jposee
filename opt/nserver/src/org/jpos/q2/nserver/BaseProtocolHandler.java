8338d72df65828e3c1bf694dc9cad73a2865902a
vsalaman <vsalaman@12794a80-bd30-0410-a029-31405052ec81>
2009-08-09 06:25:10 +0000

    - Refactoring of ProtocolHandler interface. - Moved functionallity from the mostly static DefaultISOMsgDecoder to the mostly configurable BaseProtocolHandler :) - Renamed the ATHHandler to BASE24TCPHandler. - Added message trailer support to the protocol handlers.

--- opt/nserver/src/org/jpos/q2/nserver/BaseProtocolHandler.java
+++ opt/nserver/src/org/jpos/q2/nserver/BaseProtocolHandler.java
@@ -18,6 +18,8 @@
 package org.jpos.q2.nserver;
 
 import org.apache.mina.core.buffer.IoBuffer;
+import org.apache.mina.core.session.IoSession;
+import org.apache.mina.filter.codec.ProtocolDecoderOutput;
 import org.jpos.core.Configuration;
 import org.jpos.core.ConfigurationException;
 import org.jpos.iso.ISOException;
@@ -26,27 +28,76 @@ import org.jpos.iso.ISOMsg;
 import org.jpos.iso.ISOPackager;
 import org.jpos.iso.header.BaseHeader;
 import org.jpos.util.Logger;
+import static org.jpos.q2.nserver.DecoderCtx.*;
 
 import java.io.IOException;
+import java.util.Arrays;
 
 /**
  * @author Victor Salaman (vsalaman@gmail.com)
  */
 public abstract class BaseProtocolHandler implements ProtocolHandler
 {
-    protected ISOPackager packager;
+    private ISOPackager packager;
     protected byte[] header = null;
-    protected boolean overrideHeader;
-    protected boolean useZeroLengthAsKeepalive=false;
-    protected int maxPacketLength = 100000;
+    private boolean overrideHeader;
+    private boolean useZeroLengthAsKeepalive=false;
+    private int maxPacketLength = 100000;
 
-    protected Configuration configuration;
-    protected Logger logger;
-    protected String realm;
+    private Configuration configuration;
+    private Logger logger;
+    private String realm;
 
     public void setPackager(ISOPackager packager)
     {
         this.packager = packager;
+    }
+
+    public boolean isOverrideHeader()
+    {
+        return overrideHeader;
+    }
+
+    void setOverrideHeader(boolean overrideHeader)
+    {
+        this.overrideHeader = overrideHeader;
+    }
+
+    public Configuration getConfiguration()
+    {
+        return configuration;
+    }
+
+    public void setConfiguration(Configuration configuration) throws ConfigurationException
+    {
+        this.configuration = configuration;
+        setOverrideHeader(configuration.getBoolean("override-header", false));
+    }
+
+    public Logger getLogger()
+    {
+        return logger;
+    }
+
+    void setLogger(Logger logger)
+    {
+        this.logger = logger;
+    }
+
+    public void setLogger(Logger logger, String realm)
+    {
+        setLogger(logger);
+        setRealm(realm);
+    }
+
+    public String getRealm()
+    {
+        return realm;
+    }
+
+    void setRealm(String realm)
+    {
+        this.realm = realm;
     }
 
     public ISOPackager getPackager()
@@ -74,71 +125,114 @@ public abstract class BaseProtocolHandler implements ProtocolHandler
         setHeader(header.getBytes());
     }
 
-    public boolean isUseZeroLengthAsKeepalive()
+    public DecoderCtx newDecoderContext()
     {
-        return useZeroLengthAsKeepalive;
+        DecoderCtx ctx=new DecoderCtx();
+        ctx.stage=STAGE_MSG_LEN;
+        return ctx;
     }
 
-    public void setUseZeroLengthAsKeepalive(boolean useZeroLengthAsKeepalive)
+    public boolean handleIncomingData(DecoderCtx ctx,IoSession session, IoBuffer in, ProtocolDecoderOutput out) throws Exception
     {
-        this.useZeroLengthAsKeepalive = useZeroLengthAsKeepalive;
-    }
-
-    public ISOMsg createISOMsg()
-    {
-        return packager.createISOMsg();
-    }
-
-    public int readMessageLength(IoBuffer in)
-    {
-        if (isLengthEncoded())
+        while(in.remaining()>0)
         {
-            int byteSize = getMessageLengthByteSize();
-            if (byteSize == 2)
+            switch(ctx.stage)
             {
-                return in.getShort();
-            }
-            else if (byteSize == 4)
-            {
-                return in.getInt();
+                case STAGE_MSG_LEN:
+                {
+                    if(!isLengthEncoded())
+                    {
+                        ctx.stage++;
+                        break;
+                    }
+                    if (isDataAvailable(in, getMessageLengthByteSize()))
+                    {
+                        ctx.len = readMessageLength(in);
+                        if(ctx.len==0 && isUseZeroLengthAsKeepalive())
+                        {
+                            byte[] b=new byte[getMessageLengthByteSize()];
+                            Arrays.fill(b,(byte)0);
+                            out.write(new NullMessage(b));
+                            return true;
+                        }
+                        else if(ctx.len>0 && ctx.len<= getMaxPacketLength())
+                        {
+                            ctx.stage++;
+                        }
+                        else
+                        {
+                            throw new ISOException(
+                                "receive length " +ctx.len + " seems strange - maxPacketLength = " + getMaxPacketLength());
+                        }
+                    }
+                    else return false;
+                }
+                break;
+                case STAGE_MSG_HEADER:
+                {
+                    if(!containsHeader())
+                    {
+                        ctx.stage++;
+                        break;
+                    }
+                    if (isDataAvailable(in, getHeaderLength()))
+                    {
+                        ctx.header = readHeader(in, getHeaderLength());
+                        int oldCtxLen=ctx.len;
+                        ctx.len -= ctx.header.length;
+                        if(ctx.len<0)
+                        {
+                            throw new ISOException(
+                                "Header is bigger than specified " +
+                                "(header_len+payload_len cannot be bigger than message_len),\n " +
+                                "payload size used to be: "+oldCtxLen+", is now: "+ctx.len);
+                        }
+                        ctx.stage++;
+                    }
+                    else return false;
+                }
+                break;
+                case STAGE_MSG_PAYLOAD:
+                {
+                    if (!isLengthEncoded())
+                    {
+                        ctx.payload = readStream(in);
+                        ctx.stage++;
+                    }
+                    else if (isDataAvailable(in, ctx.len))
+                    {
+                        if(ctx.len<=0 || ctx.len>getMaxPacketLength())
+                        {
+                            throw new ISOException(
+                                "payload length " +ctx.len + " seems strange - maxPacketLength = " + getMaxPacketLength());
+                        }
+                        ctx.payload = readPayload(in, ctx.len);
+                        ctx.stage++;
+                    }
+                    else return false;
+                }
+                break;
+                case STAGE_MSG_TRAILER:
+                {
+                    if(!containsTrailer())
+                    {
+                        out.write(buildMessage(ctx));
+                        ctx.stage++;
+                        return true;
+                    }
+                    final int tlen = getTrailerLength();
+                    if (isDataAvailable(in, tlen))
+                    {
+                        ctx.trailer = readTrailer(in, tlen);
+                        out.write(buildMessage(ctx));
+                        ctx.stage++;
+                        return true;
+                    }
+                    else return false;
+                }
             }
         }
-        return -1;
-    }
-
-    public int getHeaderLength()
-    {
-        return header != null ? header.length : 0;
-    }
-
-    public int getMessageLengthByteSize()
-    {
-        return -1;
-    }
-
-    public boolean isLengthEncoded()
-    {
-        return getMessageLengthByteSize() > 0;
-    }
-
-    public boolean containsHeader()
-    {
-        return getHeaderLength() > 0;
-    }
-
-    public byte[] readHeader(IoBuffer in, int len)
-    {
-        return readBytes(in, len);
-    }
-
-    public byte[] readPayload(IoBuffer in, int len)
-    {
-        return readBytes(in, len);
-    }
-
-    public byte[] readStream(IoBuffer in) throws IOException
-    {
-        return new byte[0];
+        return false;
     }
 
     public IoBuffer writeMessage(ISOMsg m) throws ISOException
@@ -173,7 +267,51 @@ public abstract class BaseProtocolHandler implements ProtocolHandler
         return buffer;
     }
 
-    private byte[] getHeader(ISOMsg m)
+    private boolean isDataAvailable(IoBuffer in, int len)
+    {
+        return in.remaining() >= len;
+    }
+
+    protected byte[] readBytes(IoBuffer in, int len)
+    {
+        byte[] b = new byte[len];
+        in.get(b);
+        return b;
+    }
+
+    private ISOMsg buildMessage(DecoderCtx ctx) throws ISOException
+    {
+        ISOMsg m = createISOMsg();
+        m.setPackager(getPackager());
+        m.setHeader(getDynamicHeader(ctx.header));
+        unpack(m, ctx.payload);
+        return m;
+    }
+
+    private void unpack(ISOMsg msg, byte[] b) throws ISOException
+    {
+        if (b.length > 0 && !shouldIgnore(msg.getHeader()))
+        {
+            msg.unpack(b);
+        }
+    }
+
+    private boolean isLengthEncoded()
+    {
+        return getMessageLengthByteSize() > 0;
+    }
+
+    private boolean containsHeader()
+    {
+        return getHeaderLength() > 0;
+    }
+
+    private boolean containsTrailer()
+    {
+        return getTrailerLength() > 0;
+    }
+
+    protected byte[] getHeader(ISOMsg m)
     {
         if (containsHeader())
         {
@@ -187,6 +325,48 @@ public abstract class BaseProtocolHandler implements ProtocolHandler
             }
         }
         return new byte[]{};
+    }
+
+    protected boolean isUseZeroLengthAsKeepalive()
+    {
+        return useZeroLengthAsKeepalive;
+    }
+
+    protected ISOMsg createISOMsg()
+    {
+        return packager.createISOMsg();
+    }
+
+    protected int getHeaderLength()
+    {
+        return header != null ? header.length : 0;
+    }
+
+    protected int getMessageLengthByteSize()
+    {
+        return -1;
+    }
+
+    protected int getTrailerLength()
+    {
+        return -1;
+    }
+
+    protected int readMessageLength(IoBuffer in)
+    {
+        if (isLengthEncoded())
+        {
+            int byteSize = getMessageLengthByteSize();
+            if (byteSize == 2)
+            {
+                return in.getShort();
+            }
+            else if (byteSize == 4)
+            {
+                return in.getInt();
+            }
+        }
+        return -1;
     }
 
     protected void writeMessageLength(IoBuffer out, ISOMsg m, int len)
@@ -205,6 +385,11 @@ public abstract class BaseProtocolHandler implements ProtocolHandler
         }
     }
 
+    protected byte[] readHeader(IoBuffer in, int len)
+    {
+        return readBytes(in, len);
+    }
+
     protected void writeHeader(IoBuffer out, ISOMsg m)
     {
         byte[] h=getHeader(m);
@@ -214,28 +399,35 @@ public abstract class BaseProtocolHandler implements ProtocolHandler
         }
     }
 
+    protected byte[] readPayload(IoBuffer in, int len)
+    {
+        return readBytes(in, len);
+    }
+
     protected void writePayload(IoBuffer out, byte[] b)
     {
         out.put(b);
     }
 
+    protected byte[] readStream(IoBuffer in) throws IOException
+    {
+        return new byte[0];
+    }
+
+
+    @SuppressWarnings({"UnusedDeclaration"})
+    protected byte[] readTrailer(IoBuffer in,int len)
+    {
+        if(containsTrailer())
+        {
+            return readBytes(in,getTrailerLength());
+        }
+        return new byte[0];
+    }
+
+    @SuppressWarnings({"UnusedDeclaration"})
     protected void writeTrailer(IoBuffer out, byte[] b)
     {
-    }
-
-    private byte[] readBytes(IoBuffer in, int len)
-    {
-        byte[] b = new byte[len];
-        in.get(b);
-        return b;
-    }
-
-    public void unpack(ISOMsg msg, byte[] b) throws ISOException
-    {
-        if (b.length > 0 && !shouldIgnore(msg.getHeader()))
-        {
-            msg.unpack(b);
-        }
     }
 
     @SuppressWarnings({"UnusedDeclaration"})
@@ -250,55 +442,8 @@ public abstract class BaseProtocolHandler implements ProtocolHandler
         return false;
     }
 
-    public ISOHeader getDynamicHeader(byte[] image)
+    protected ISOHeader getDynamicHeader(byte[] image)
     {
         return image != null ? new BaseHeader(image) : null;
-    }
-
-    public boolean isOverrideHeader()
-    {
-        return overrideHeader;
-    }
-
-    public void setOverrideHeader(boolean overrideHeader)
-    {
-        this.overrideHeader = overrideHeader;
-    }
-
-    public Configuration getConfiguration()
-    {
-        return configuration;
-    }
-
-    public void setConfiguration(Configuration configuration) throws ConfigurationException
-    {
-        this.configuration = configuration;
-        setOverrideHeader(configuration.getBoolean("override-header", false));
-    }
-
-    public Logger getLogger()
-    {
-        return logger;
-    }
-
-    public void setLogger(Logger logger)
-    {
-        this.logger = logger;
-    }
-
-    public void setLogger(Logger logger, String realm)
-    {
-        setLogger(logger);
-        setRealm(realm);
-    }
-
-    public String getRealm()
-    {
-        return realm;
-    }
-
-    public void setRealm(String realm)
-    {
-        this.realm = realm;
     }
 }
