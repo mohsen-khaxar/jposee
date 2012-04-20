100ba46002f8f3f5197b4c838498c7d2c78dd1ef
arevilla <arevilla@12794a80-bd30-0410-a029-31405052ec81>
2012-04-20 18:26:25 +0000

    delete schema revision if exists

--- trunk/opt/eeuser/sql/upgrade_to_r432.sql
+++ trunk/opt/eeuser/sql/upgrade_to_r432.sql
@@ -3,5 +3,6 @@ ALTER TABLE eeuser ADD verified char(1) DEFAULT 'N';
 ALTER TABLE eeuser CHANGE active active char(1) DEFAULT 'N';
 ALTER TABLE eeuser CHANGE deleted deleted char(1) DEFAULT 'N';
 
+DELETE FROM sysconfig WHERE id = 'SCHEMA_REVISION.eeuser';
 INSERT INTO sysconfig (id, value, readPerm) VALUES ('SCHEMA_REVISION.eeuser', '432', 'admin');
 
